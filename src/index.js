751a3ddd2abba118866e3e0423c48033449ba4d2
Dmitry Tselinko <dmitry.tselinko@gmail.com>
2020-03-17 23:09:16 +0100

    added static content

--- src/index.js
+++ src/index.js
@@ -2,28 +2,32 @@
 
 const express = require('express');
 const http = require('http');
+const path = require('path');
 
 const logger = require('./logger');
 
 // Constants
 const PORT = process.env.PORT || 8080;
 
+const TEMPLATES_PATH = path.join(__dirname, 'static/templates');
+
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h2>Hello App</h2><a href="/success">Success Logs</a>&nbsp;|&nbsp;<a href="/warning">Warning Logs</a>&nbsp;|&nbsp;<a href="/error">Error Logs</a>');
+  res.sendFile(path.join(TEMPLATES_PATH, 'index.html'));
 });
+app.use('/static', express.static(__dirname + '/static'));
 app.get('/success', (req, res) => {
   logger.info('Inline success logs were sent');
-  res.send('<a href="/">Home</a><br /><p style="color: green">Success logs were generated</p>');
+  res.sendFile(path.join(TEMPLATES_PATH, 'success.html'));
 });
 app.get('/warning', (req, res) => {
   logger.warn('Inline warn logs were sent');
-  res.send('<a href="/">Home</a><br /><p style="color: green">Warning logs were generated</p>');
+  res.sendFile(path.join(TEMPLATES_PATH, 'warning.html'));
 });
 app.get('/error', (req, res) => {
   logger.error('Inline error logs were sent');
-  res.send('<a href="/">Home</a><br /><p style="color: green">Error logs were generated</p>');
+  res.sendFile(path.join(TEMPLATES_PATH, 'error.html'));
 });
 
 const server = http.createServer(app);
